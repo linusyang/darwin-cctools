8fbf0c12605d4517833cb134f1a5e2ec8ecbe587
Linus Yang <laokongzi@gmail.com>
2012-09-06 21:16:39 +0800

    [fix] ios build dlfcn.h header

--- build-ios.sh
+++ build-ios.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 cd "$(dirname "$0")"
+rm -rf ./prelib ./ldid-host
 tar zxf prelib.tgz
 IOSVER="4.2"
 PREFIX="/Developer"
@@ -11,15 +12,18 @@ export DEVROOT="${PREFIX}/Platforms/iPhoneOS.platform/Developer"
 export SDKROOT="${DEVROOT}/SDKs/iPhoneOS${IOSVER}.sdk"
 CC="${DEVROOT}/usr/bin/gcc" CXX="${DEVROOT}/usr/bin/g++" \
 LD="${DEVROOT}/usr/bin/ld" CPP="/usr/bin/cpp" \
-CFLAGS="-arch armv6 -I${SDKROOT}/usr/include -I./prelib/usr/include -isysroot ${SDKROOT}" \
-LDFLAGS="-miphoneos-version-min=3.0 -L${SDKROOT}/usr/lib -L./prelib/usr/lib -isysroot ${SDKROOT}" \
+CFLAGS="-arch armv6 -I./prelib/usr/include -I${SDKROOT}/usr/include -isysroot ${SDKROOT}" \
+LDFLAGS="-miphoneos-version-min=3.0 -L./prelib/usr/lib -L${SDKROOT}/usr/lib -isysroot ${SDKROOT}" \
 CXXFLAGS="${CFLAGS}" ./configure --prefix=/usr --target=arm-apple-darwin10 --host=arm-apple-darwin10
 make
 rm -rf ./_install
 DESTDIR=./_install make install
 make clean
 g++ -O2 -o ./ldid-host ldid/ldid.cpp -Ildid -x c ldid/lookup2.c ldid/sha1.c
-${DEVROOT}/usr/bin/g++ -O2 -arch armv6 -o ./_install/usr/bin/ldid ldid/ldid.cpp -isysroot "${SDKROOT}" -miphoneos-version-min=3.0 -L"${SDKROOT}/usr/lib" -I"${SDKROOT}/usr/include" -Ildid -x c ldid/lookup2.c ldid/sha1.c
+${DEVROOT}/usr/bin/g++ -O2 -arch armv6 -o ./_install/usr/bin/ldid ldid/ldid.cpp \
+        -isysroot "${SDKROOT}" -miphoneos-version-min=3.0 \
+        -L"${SDKROOT}/usr/lib" -I"${SDKROOT}/usr/include" \
+        -Ildid -x c ldid/lookup2.c ldid/sha1.c
 export CODESIGN_ALLOCATE="${DEVROOT}/usr/bin/codesign_allocate"
 for i in ./_install/usr/bin/*; do ./ldid-host -S $i; done;
 rm -rf ./prelib ./ldid-host
